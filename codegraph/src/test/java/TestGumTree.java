4c2f19e92086c6c4c91b94fd116389b45fa4bcfe
ssmingz <jazz302465@163.com>
2022-11-08 11:43:21 +0800

    change folder codegraph to module

--- codegraph/src/test/java/TestGumTree.java
+++ codegraph/src/test/java/TestGumTree.java
@@ -9,8 +9,8 @@ import static org.junit.Assert.assertTrue;
 public class TestGumTree {
     @Test
     public void testGumTreeDiff() throws Exception {
-        String srcFile = System.getProperty("user.dir") + "/codegraph/src/test/resources/0b20e4026c_d87861eb35/buggy_version/DashboardCommand.java";
-        String dstFile = System.getProperty("user.dir") + "/codegraph/src/test/resources/0b20e4026c_d87861eb35/fixed_version/DashboardCommand.java";
+        String srcFile = "src/test/resources/0b20e4026c_d87861eb35/buggy_version/DashboardCommand.java";
+        String dstFile = "src/test/resources/0b20e4026c_d87861eb35/fixed_version/DashboardCommand.java";
         AstComparator diff = new AstComparator();
         Diff editScript = diff.compare(FileIO.readStringFromFile(srcFile), FileIO.readStringFromFile(dstFile));
 
---
+++ codegraph/target/test-classes/input4test.java
@@ -0,0 +1,43 @@
+import org.eclipse.jdt.core.dom.ASTNode;
+
+import java.util.ArrayList;
+
+public abstract class Node {
+    protected String _fileName;
+    protected int _startLine;
+    protected int _endLine;
+    /**
+     * original AST node in the JDT abstract tree model
+     * NOTE: AST node does not support serialization
+     */
+    protected transient ASTNode _astNode;
+    /**
+     * parent node in the abstract syntax tree
+     */
+    protected Node _parent;
+
+    /**
+     * @param oriNode   : original abstract syntax tree node in the JDT model
+     * @param fileName  : source file name
+     * @param startLine : start line number of the node in the original source file
+     * @param endLine   : end line number of the node in the original source file
+     */
+    public Node(ASTNode oriNode, String fileName, int startLine, int endLine) {
+        this(oriNode, fileName, startLine, endLine, null);
+    }
+
+    /**
+     * @param oriNode   : original abstract syntax tree node in the JDT model
+     * @param fileName  : source file name (with absolute path)
+     * @param startLine : start line number of the node in the original source file
+     * @param endLine   : end line number of the node in the original source file
+     * @param parent    : parent node in the abstract syntax tree
+     */
+    public Node(ASTNode oriNode, String fileName, int startLine, int endLine, Node parent) {
+        if (oriNode != null) {
+            _startLine = startLine;
+            _endLine = endLine + _startLine;
+            testvar = _endLine + _startLine;
+        }
+    }
+}
