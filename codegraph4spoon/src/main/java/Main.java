8c67700ffbee67d41309ef246752e1f81451ec95
jiachen <jiachen_han@qq.com>
2023-09-23 20:28:27 +0800

    <doc> Add instructions for use

--- codegraph4spoon/src/main/java/Main.java
+++ codegraph4spoon/src/main/java/Main.java
@@ -2,20 +2,20 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("Please given the arguments");
-            System.err.println("\ttest : test on datasets.");
-            System.err.println("\textract : extract patterns.");
-            System.err.println("\tdefect : defect faults on project.");
+            System.err.println("\t--test : test on datasets.");
+            System.err.println("\t--extract : extract patterns.");
+            System.err.println("\t--defect : defect faults on project.");
             System.exit(1);
         }
 
         switch (args[0]) {
-            case "test":
+            case "--test":
                 RunOnDataset.main(args);
                 break;
-            case "extract":
+            case "--extract":
                 ExtractPattern.main(args);
                 break;
-            case "defect":
+            case "--defect":
                 DefectFaults.main(args);
                 break;
             default:
--- codegraph4spoon/src/main/java/config.md
+++ codegraph4spoon/src/main/java/config.md
@@ -37,11 +37,32 @@ For example:
 The code change in dataset should be as similar as possible.
 We use caseA to generate patterns and apply them on caseB in one group.
 
+To run FixGen on dataset, please add `--test` as the only argument.
+
+For example, if you build whole project with all dependencies as a jar file, you can run it as follows:
+
+```
+    java -jar FixGen.jar --test
+```
+
+
 ### Defect Potential Faults on Projects
 
 To avoid incorrect modification, please use patterns extracted from the same project
 to defect potential faults.
 
+To detect potential faults on projects using FixGen, 
+Please add `--extract` to extract patterns from historical modification and save it to a directory.
+```
+    java -jar FixGen.jar --extract
+```
+Then add `--defect` to detect potential faults on projects using the extracted patterns.
+```
+    java -jar FixGen.jar --defect
+```
+
+
+All the configurable file paths are in `resources/option.yml` in subModule `codegraph4spoon`.
 
 
 
